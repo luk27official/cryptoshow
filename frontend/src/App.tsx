9afc856df0e37ddfd694d93238cea23b56d73c44
Lukas Polak <admin@lukaspolak.cz>
2025-02-23 22:31:49 +0100

    Improve frontend, enable task submission by PDB IDs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,65 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [pdbCode, setPdbCode] = useState('');
+  const [taskId, setTaskId] = useState('');
+  const [resultData, setResultData] = useState<any>({}); // TODO: add proper types
+
+  const handleSubmit = async () => {
+    const data = await fetch(`./api/calculate/${pdbCode}`)
+      .then(response => response.json()); // TODO: add proper types and error handling
+
+    console.log("submitting...", data);
+
+    setTaskId(data["task_id"]);
+    poll(data["task_id"]);
+  };
+
+  const poll = async (tId: string) => {
+    const data = await fetch(`./api/task-status/${tId}`)
+      .then(response => response.json()); // TODO add proper types and error handling
+
+    console.log("polling...", data);
+
+    if (data["status"] === 'SUCCESS') { // TODO: add more status, enable showing intermediate results
+      setResultData(data["result"]);
+    } else if (data["status"] === 'PENDING') {
+      setTimeout(() => poll(tId), 1000);
+    } else {
+      setResultData(data["status"]);
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>CryptoShow</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <span>Input a PDB code: </span>
+        <input type="text" value={pdbCode} onChange={(e) => setPdbCode(e.target.value)} />
+        &nbsp;
+        <button onClick={() => handleSubmit()}>Submit</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {taskId &&
+        <div>
+          <h3>Task ID:</h3>
+          <p>{taskId}</p>
+        </div>
+      }
+      {resultData["status"] &&
+        <div>
+          <h3>Result:</h3>
+          <p>{resultData["status"]}</p>
+          <ul>
+            {resultData["prediction"].map((value: number, index: number) => (
+              <li key={index}>{value.toFixed(5)}</li>
+            ))}
+          </ul>
+        </div>
+      }
     </>
   );
 }
