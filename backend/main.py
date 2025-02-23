50a710c7beee8d023cc05660551c67fd28e2864b
Lukas Polak <admin@lukaspolak.cz>
2025-02-23 16:53:18 +0100

    Add CUDA support, make tasks async via Celery

--- backend/main.py
+++ backend/main.py
@@ -18,10 +18,8 @@ import biotite.structure.io.pdbx as pdbx
 from biotite.structure.io.pdbx import get_structure
 from biotite.sequence import ProteinSequence
 
-
-from .esm2_generator import compute_esm2
-from .cb import compute_prediction
-from .tasks import process_3d_structure
+from .tasks import celery_app
+from celery.result import AsyncResult
 
 app = FastAPI()
 uvicorn_logger = logging.getLogger("uvicorn.error")
@@ -34,48 +32,26 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/{pdb_id}")
+@app.get("/debug/{pdb_id}")
 async def run_pdb_id(pdb_id: str):
 
-    logger.warning(f"new request: GET /{pdb_id}")
+    logger.warning(f"debug: GET /{pdb_id}")
 
     print("Converting the file to a sequence")
     # download the pdb
     cif_file_path = rcsb.fetch(pdb_id, "cif", f"/app/data/inputs/")
     cif_file = pdbx.CIFFile.read(cif_file_path)
 
-    chain_id = "A"
+    chain_id = "A"  # TODO: change this
 
     protein = get_structure(cif_file, model=1)
     protein = protein[(protein.atom_name == "CA") & (protein.element == "C") & (protein.chain_id == chain_id)]
 
     seq = "".join([ProteinSequence.convert_letter_3to1(residue.res_name) for residue in protein])
-    with open(f"/app/data/inputs/{pdb_id}.fasta", "w") as f:
-        f.write(seq)
 
     print(f"Saved sequence file to /app/data/inputs/{pdb_id}.fasta")
 
-    # run the ml model
-    if not DEBUG:
-        try:
-            compute_esm2(f"/app/data/inputs/{pdb_id}.fasta", f"/app/data/outputs/{pdb_id}.npy")
-        except Exception as e:
-            logger.error(f"Error running ESM2 model: {e}")
-            return {"error": str(e)}
-
-        print(f"Saved ESM2 embeddings to /app/data/outputs/{pdb_id}.npy")
-
-    # run the cryptobench model
-    if DEBUG:
-        pred = [[x, 1 - x] for x in [random.random() for _ in range(len(seq))]]
-    else:
-        try:
-            pred = compute_prediction(f"/app/data/outputs/{pdb_id}.npy")
-        except Exception as e:
-            logger.error(f"Error running ESM2 model: {e}")
-            return {"error": str(e)}
-
-    print(f"Prediction: {pred}")
+    pred = [[x, 1 - x] for x in [random.random() for _ in range(len(seq))]]
 
     return {
         "status": f"Prediction run succesfully for {pdb_id}. Available at /app/data/outputs/{pdb_id}.npy",
@@ -83,17 +59,25 @@ async def run_pdb_id(pdb_id: str):
     }
 
 
-@app.post("/process/{id}")
+@app.get("/calculate/{pdb_id}")
+async def calculate(pdb_id: str):
+    """Calculates the prediction for a given PDB ID."""
+    task: AsyncResult = celery_app.send_task("celery_app.process_esm2_cryptobench", args=(pdb_id,))
+
+    return {"task_id": task.id}
+
+
+@app.get("/process/{id}")
 async def process_string_test(id: str):
     """Uploads a string and starts processing."""
     test_string = id + "_hi"
 
-    task = process_3d_structure.delay(test_string)  # Send job to Celery
+    task: AsyncResult = celery_app.send_task("celery_app.process_string_test", args=(test_string,))
     return {"task_id": task.id}
 
 
 @app.get("/task-status/{task_id}")
 def get_status(task_id: str):
     """Check the status of a processing task."""
-    task_result = process_3d_structure.AsyncResult(task_id)
+    task_result: AsyncResult = celery_app.AsyncResult(task_id)
     return {"status": task_result.state, "result": task_result.result}
--- backend/tasks.py
+++ backend/tasks.py
@@ -2,16 +2,61 @@ from celery import Celery
 import torch
 import time
 
+import biotite.database.rcsb as rcsb
+import biotite.structure.io.pdbx as pdbx
+from biotite.structure.io.pdbx import get_structure
+from biotite.sequence import ProteinSequence
+
+from esm2_generator import compute_esm2
+from cb import compute_prediction
+
 celery_app = Celery("celery_app", broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 
 
-@celery_app.task(name="process_3d_structure")
-def process_3d_structure(file_path: str):
+@celery_app.task(name="celery_app.process_string_test")
+def process_string_test(string: str):
     """Run ML inference on the uploaded 3D structure."""
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Simulate ML model inference
     time.sleep(3)  # Fake processing time
-    result = {"device": device, "prediction": "3D structure processed"}
+    result = {"device": device, "string": string}
 
     return result
+
+
+@celery_app.task(name="celery_app.process_esm2_cryptobench")
+def process_esm2_cryptobench(pdb_id: str):
+    """Run ESM2 and CryptoBench models on the uploaded 3D structure."""
+    cif_file_path = rcsb.fetch(pdb_id, "cif", f"/app/data/inputs/")
+    cif_file = pdbx.CIFFile.read(cif_file_path)
+
+    chain_id = "A"  # TODO: change this
+
+    protein = get_structure(cif_file, model=1)
+    protein = protein[(protein.atom_name == "CA") & (protein.element == "C") & (protein.chain_id == chain_id)]
+
+    seq = "".join([ProteinSequence.convert_letter_3to1(residue.res_name) for residue in protein])
+    with open(f"/app/data/inputs/{pdb_id}.fasta", "w") as f:
+        f.write(seq)
+
+    print(f"Saved sequence file to /app/data/inputs/{pdb_id}.fasta")
+
+    # run the ml model
+    try:
+        compute_esm2(f"/app/data/inputs/{pdb_id}.fasta", f"/app/data/outputs/{pdb_id}.npy")
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
+
+    print(f"Saved ESM2 embeddings to /app/data/outputs/{pdb_id}.npy")
+
+    # run the cryptobench model
+    try:
+        pred = compute_prediction(f"/app/data/outputs/{pdb_id}.npy")
+    except Exception as e:
+        return {"status": "error", "error": str(e)}
+
+    return {
+        "status": f"Prediction run succesfully for {pdb_id}. Available at /app/data/outputs/{pdb_id}.npy",
+        "prediction": [float(p[1]) for p in pred],
+    }
