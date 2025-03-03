Fix CryptoBench CPU computation

backend/cb_small/compute_score.py
@@ -8,7 +8,7 @@ def compute_prediction(embedding_path: os.PathLike):
     PATH = "/app/cryptobench-small/model-650M.pt"
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    model = torch.jit.load(PATH)
+    model = torch.jit.load(PATH, map_location=device)
     model.eval()
 
     logits = model(torch.tensor(np.load(embedding_path), dtype=torch.float32).to(device)).squeeze()