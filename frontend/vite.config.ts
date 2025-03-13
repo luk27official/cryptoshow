71ce48822522656c7accb7466a0c91abd79e8f3f
Lukas Polak <admin@lukaspolak.cz>
2025-03-13 21:14:29 +0100

    Fix quotes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost',
+      "/api": {
+        target: "http://localhost",
         changeOrigin: true,
       },
-      '/ws': {
-        target: 'ws://localhost',
+      "/ws": {
+        target: "ws://localhost",
         ws: true,
       }
     }
