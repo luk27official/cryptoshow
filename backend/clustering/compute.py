063e81d0195d9ee7cd6197c0be544167b79c855e
Lukas Polak <admin@lukaspolak.cz>
2025-03-13 20:23:36 +0100

    Small clustering tweak

--- backend/clustering/compute.py
+++ backend/clustering/compute.py
@@ -8,11 +8,11 @@ def compute_clusters(points: list[list[float]], prediction_scores: list[float]):
     scores_array = np.array(prediction_scores).reshape(-1, 1)
     stacked = np.hstack((points_array, scores_array))  # Combine coordinates with scores
 
-    high_score_mask = stacked[:, 3] > 0.5  # TODO: tweak this
+    high_score_mask = stacked[:, 3] > 0.65  # TODO: tweak this
     high_score_points = stacked[high_score_mask][:, :3]  # Extract only (x, y, z) coordinates
 
     eps = 5.0  # Max distance for neighbors (adjust as needed)  # TODO: tweak this
-    min_samples = 2  # Minimum points to form a cluster         # TODO: tweak this
+    min_samples = 3  # Minimum points to form a cluster         # TODO: tweak this
     dbscan = DBSCAN(eps=eps, min_samples=min_samples)
     labels = dbscan.fit_predict(high_score_points)
 
