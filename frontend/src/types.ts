1d5f5b8955145dda0699174c95076239d16e5394
Lukas Polak <admin@lukaspolak.cz>
2025-03-14 18:05:28 +0100

    Fix results type for TypeScript

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,7 +1,9 @@
 export type CryptoBenchResult = {
     status: string;
-    prediction?: number[];
-    pockets?: number[];
-    sequence?: string[];
-    residue_ids?: string[];
+    prediction: number[];
+    pockets: number[];
+    sequence: string[];
+    residue_ids: string[];
+    input_structure: string;
+    task_id: string;
 };
