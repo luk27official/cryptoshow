e9a603c94c55d207eca23154c61776dbd9360e7a
Lukas Polak <admin@lukaspolak.cz>
2025-04-19 22:57:09 +0200

    Add PyMOL export command

--- frontend/src/components/ResultTableRow.tsx
+++ frontend/src/components/ResultTableRow.tsx
@@ -85,6 +85,23 @@ const PocketDetails = ({ pocket }: PocketDetailsProps) => {
     const predictionString = pocket.prediction.map((e) => e.toFixed(3)).join(", ");
     const residueIds = pocket.residue_ids.join(", ");
 
+    const { cryptoBenchResult } = useAppContext();
+    const structureName = cryptoBenchResult!.structure_name;
+
+    const residuesByChain: { [key: string]: string[]; } = {};
+    pocket.residue_ids.forEach(residueId => {
+        const [chain, resNum] = residueId.split("_");
+        if (!residuesByChain[chain]) {
+            residuesByChain[chain] = [];
+        }
+        residuesByChain[chain].push(resNum);
+    });
+
+    const chainSelectors = Object.entries(residuesByChain)
+        .map(([chain, resNums]) => `(chain ${chain} and resi ${resNums.join("+")})`);
+
+    const pymolCommand = `select s, ${structureName} and ( ${chainSelectors.join(" or ")} )`;
+
     return (
         <div className="pocket-details">
             <div className="pocket-info">
@@ -95,6 +112,10 @@ const PocketDetails = ({ pocket }: PocketDetailsProps) => {
                 <div className="residue-ids">
                     <span className="info-label">Residue IDs:</span>
                     <span className="info-value truncate-text">{residueIds}</span>
+                </div>
+                <div className="pymol-export">
+                    <span className="info-label">PyMOL visualization:</span>
+                    <span className="info-value code-block">{pymolCommand}</span>
                 </div>
             </div>
         </div>
