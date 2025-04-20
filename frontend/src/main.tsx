d813f26c702a37cd6f23e00d3926dc84975d9c2f
Lukas Polak <admin@lukaspolak.cz>
2025-04-21 00:31:47 +0200

    Only use Sentry if the env vars are defined

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN
-});
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
