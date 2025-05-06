923c2bf11c11d7996f61fa59af8e23be0221003a
Lukas Polak <admin@lukaspolak.cz>
2025-05-06 18:43:03 +0200

    Add Elixir CZ

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,6 +1,8 @@
 import "./AboutPage.css";
 
 function AboutPage() {
+    const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
     return (
         <div className="about-container">
             <h2>About CryptoShow</h2>
@@ -16,6 +18,14 @@ function AboutPage() {
             </p>
             <p>
                 Contact us at <i>admin [at] lukaspolak.cz</i>
+            </p>
+            <div className="logo-container">
+                <a href="https://www.elixir-czech.cz/services" target="_blank" rel="noopener noreferrer">
+                    <img src={isDarkMode ? "/elixir-czech-dark.svg" : "/elixir-czech.svg"} alt="ELIXIR CZ Logo" className="elixir-logo" width="120rem" height="96rem" />
+                </a>
+            </div>
+            <p>
+                Part of this work was carried out with the support of <a href="https://www.elixir-czech.cz/services" target="_blank" rel="noopener noreferrer">ELIXIR CZ</a> Research Infrastructure (ID LM2023055, MEYS CR).
             </p>
         </div>
     );
