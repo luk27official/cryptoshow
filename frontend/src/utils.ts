d6263491698520cda16859450afa83112a7a6f4d
Lukas Polak <admin@lukaspolak.cz>
2025-05-08 21:45:29 +0200

    Add more colors to the palette

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -13,6 +13,7 @@ export const getColor = (value: number) => {
 };
 
 // https://davidmathlogic.com/colorblind/#%23332288-%23117733-%2344AA99-%2388CCEE-%23DDCC77-%23CC6677-%23AA4499-%23882255
+// extended by some colors
 const defaultColors = [
     0x332288,
     0x117733,
@@ -22,6 +23,24 @@ const defaultColors = [
     0xcc6677,
     0xaa4499,
     0x882255,
+    0xff0000,
+    0x00ff00,
+    0x0000ff,
+    0xffa500,
+    0x800080,
+    0xffc0cb,
+    0xa52a2a,
+    0x808080,
+    0xffff00,
+    0x00ffff,
+    0xff00ff,
+    0x00ff00,
+    0x000080,
+    0x008080,
+    0x800000,
+    0x808000,
+    0xc0c0c0,
+    0x00ffff,
 ];
 
 export const COMPLETED_TASKS_KEY = "completedTasks";
