fd6f1565b19f3695174422f54e7e7e4b50736905
Lukas Polak <admin@lukaspolak.cz>
2025-05-19 14:51:06 +0200

    Remove root paths as we run the backend as a non-root user

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -f /app/cryptobench-small/model-650M.pt ]; then
     wget --progress=dot:giga https://github.com/skrhakv/TinyCryptoBench/raw/3adde8c5489f5f52b3df6c5b1bbd80898ea3a2c2/data/model-650M.pt -O /app/cryptobench-small/model-650M.pt
 fi
 
-for user_dir in /home/cryptoshow /root; do
+for user_dir in /home/cryptoshow; do
     cache_dir="$user_dir/.cache/torch/hub/checkpoints"
     mkdir -p "$cache_dir"
 
