890bf44b1c4ff0756203c43f022f5fdcd5822c6f
Lukas Polak <admin@lukaspolak.cz>
2025-05-26 16:28:08 +0200

    Fix finetuned model URL

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir -p /app/data/jobs
 
 if [ ! -f /app/cryptobench-small/model-650M-finetuned.pt ]; then
-    wget --progress=dot:giga localhost -O /app/cryptobench-small/model-650M-finetuned.pt # TODO: replace with actual URL 
+    wget --progress=dot:giga https://owncloud.cesnet.cz/index.php/s/mPo9NCWMBouwVAo/download -O /app/cryptobench-small/model-650M-finetuned.pt
 fi
 
 exec "$@"
