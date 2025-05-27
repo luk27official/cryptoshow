2eaba85dbfdc89910366758031159f5eb2d1a0ee
Lukas Polak <lukas.polak@merck.com>
2025-05-27 16:29:10 +0200

    Add a SHA-256 check for the downloaded model file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,31 @@
 
 mkdir -p /app/data/jobs
 
-if [ ! -f /app/cryptobench-small/model-650M-finetuned.pt ]; then
-    wget --progress=dot:giga https://owncloud.cesnet.cz/index.php/s/mPo9NCWMBouwVAo/download -O /app/cryptobench-small/model-650M-finetuned.pt
+MODEL_FILE=/app/cryptobench-small/model-650M-finetuned.pt
+
+if [ ! -f "$MODEL_FILE" ]; then
+    wget --no-check-certificate --progress=dot:giga https://owncloud.cesnet.cz/index.php/s/mPo9NCWMBouwVAo/download -O "$MODEL_FILE"
+
+    # This is just a sanity check that we downloaded the correct model file - if needed, 
+    # set the EXPECTED_HASH to an empty string to skip this verification.
+    ACTUAL_HASH=$(sha256sum "$MODEL_FILE" | awk '{print $1}')
+    EXPECTED_HASH="ca2cb0d6a666446cf2a9671c8cccbbede445c0667b385b0a67e6f81b13713f25"
+
+    if [ -n "$EXPECTED_HASH" ]; then
+        ACTUAL_HASH=$(sha256sum "$MODEL_FILE" | awk '{print $1}')
+        
+        if [ "$ACTUAL_HASH" = "$EXPECTED_HASH" ]; then
+            echo "✅ Hash OK"
+        else
+            echo "❌ Hash MISMATCH"
+            echo "Expected: $EXPECTED_HASH"
+            echo "Actual:   $ACTUAL_HASH"
+            echo "This means potential security violation! Consider checking the file URL..."
+            exit 1
+        fi
+    else
+        echo "⚠️  Skipping hash check: no expected hash provided."
+    fi
 fi
 
 exec "$@"
