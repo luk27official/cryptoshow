da92d8f2d01eb67b1f25b2340ab98d0856d9368a
Lukas Polak <admin@lukaspolak.cz>
2025-09-03 11:21:15 +0200

    Pause Docker compose when renewing certificates

--- frontend/nginx/renew_certs.sh
+++ frontend/nginx/renew_certs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # This script is used to renew SSL certificates using Certbot and copy them to a directory for use in an Nginx server.
 # It is meant to be run as a cron job or manually when needed - but make sure to run it on the HOST machine, not in the container.
+# Make sure to modify before using (e.g. docker-compose profiles!)
 set -e
 
+PROJECT_ROOT="$(dirname "$0")/../.."
+
+echo "Stopping docker-compose services..."
+cd "$PROJECT_ROOT" && docker-compose down
+
+echo "Renewing SSL certificates..."
 sudo certbot renew --quiet
 
 SRC="/etc/letsencrypt/live/$DOMAIN"
@@ -22,6 +29,9 @@ cp -L "$SRC/fullchain.pem" "$DST/"
 cp -L "$SRC/privkey.pem" "$DST/"
 
 echo "Certs updated at $DST"
+
+echo "Starting docker-compose services..."
+cd "$PROJECT_ROOT" && docker-compose --profile cpu --profile monitoring up -d
 
 echo "Removing maintenance flag file at $MAINTENANCE/maintenance.flag"
 
